f97f94e20061aa4b42b68b1c468879fa8fdbceac
AdelinChelariu <67071192+AdelinChelariu@users.noreply.github.com>
2024-11-06 21:25:30 +0200

    Added new Class

---
+++ Code/MagazinPieseAuto/src/Angajat.java
@@ -0,0 +1,11 @@
+public class Angajat {
+    private int id_angajat;
+    private String nume;
+    private String prenume;
+    private String functie;
+    private String departament;
+    private String telefon;
+    private String email;
+
+    // Constructori, getteri, setteri, toString()
+}
